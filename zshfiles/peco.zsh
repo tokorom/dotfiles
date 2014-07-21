8c8b8e50ad123a9081f5de0f95838a66a181c500
tokorom <tokorom@gmail.com>
2014-07-21 22:24:09 +0900

    Add ll-peco

--- zshfiles/peco.zsh
+++ zshfiles/peco.zsh
@@ -3,15 +3,25 @@
 function peco-select-history() {
     local tac
     if which tac > /dev/null; then
-        tac="tac"
+        tac="\tac"
     else
-        tac="tail -r"
+        tac="\tail -r"
     fi
     BUFFER=$(\history -n 1 | \
         eval $tac | \
-        peco --query "$LBUFFER")
+        \peco --query "$LBUFFER")
     CURSOR=$#BUFFER
     zle clear-screen
 }
 zle -N peco-select-history
 bindkey '^r' peco-select-history
+
+# ll
+
+function ll-peco() {
+    SELECTED=$(\ls -la | \peco)
+    EXTRACTED=$(\echo ${SELECTED} | sed -e "s/.*\s\(\S\+\)*$/\1/")
+    eval \echo ${EXTRACTED}
+    eval \echo ${EXTRACTED} | tr -d '\n' | \pbcopy
+}
+zle -N ll-peco
