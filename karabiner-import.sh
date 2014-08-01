13f08a7c525883a8aeb4b8cdbb0ea8293791bb22
tokorom <tokorom@gmail.com>
2014-08-01 13:31:08 +0900

    Add Karabiner settings

---
+++ karabiner-import.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.extra_cursor_optionL_kjhl_tokorom 1
+/bin/echo -n .
+$cli set remap.hhkmode_optD_to_f11_tokorom 1
+/bin/echo -n .
+$cli set option.emacsmode_controlM_tokorom 1
+/bin/echo -n .
+$cli set option.emacsmode_controlI_tokorom 1
+/bin/echo -n .
+$cli set remap.hhkmode_opt1_optEqual_to_f1_f12_tokorom 1
+/bin/echo -n .
+/bin/echo
